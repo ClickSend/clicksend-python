a6cfeeed977dfbcd068efdfb686a393e4b9bead1
Support <Support@ClickSend.com>
2019-10-15 01:42:06 +0000

    New python SDK Generated

--- clicksend_client/api/sms_api.py
+++ clicksend_client/api/sms_api.py
@@ -936,6 +936,105 @@ class SMSApi(object):
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def sms_receipt_read_by_message_id_put(self, message_id, **kwargs):  # noqa: E501
+        """Mark specific delivery receipt as read  # noqa: E501
+
+        Mark specific delivery receipt as read  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.sms_receipt_read_by_message_id_put(message_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str message_id: The message ID you want to mark as read (required)
+        :return: str
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.sms_receipt_read_by_message_id_put_with_http_info(message_id, **kwargs)  # noqa: E501
+        else:
+            (data) = self.sms_receipt_read_by_message_id_put_with_http_info(message_id, **kwargs)  # noqa: E501
+            return data
+
+    def sms_receipt_read_by_message_id_put_with_http_info(self, message_id, **kwargs):  # noqa: E501
+        """Mark specific delivery receipt as read  # noqa: E501
+
+        Mark specific delivery receipt as read  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.sms_receipt_read_by_message_id_put_with_http_info(message_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str message_id: The message ID you want to mark as read (required)
+        :return: str
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['message_id']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method sms_receipt_read_by_message_id_put" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'message_id' is set
+        if ('message_id' not in params or
+                params['message_id'] is None):
+            raise ValueError("Missing the required parameter `message_id` when calling `sms_receipt_read_by_message_id_put`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'message_id' in params:
+            path_params['message_id'] = params['message_id']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['BasicAuth']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/sms/receipts-read/{message_id}', 'PUT',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='str',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def sms_receipts_by_message_id_get(self, message_id, **kwargs):  # noqa: E501
         """Get a Specific Delivery Receipt  # noqa: E501
 
