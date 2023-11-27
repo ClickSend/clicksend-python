93635a28913323f18ca2b5fe40a67e55fa215fef
Support <Support@ClickSend.com>
2023-11-27 06:44:03 +0000

    New python SDK Generated

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "clicksend-client"
-VERSION = "5.0.75"
+VERSION = "5.0.78"
 # To install the library, run the following
 #
 # python setup.py install
